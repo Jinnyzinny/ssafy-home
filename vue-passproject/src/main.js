aceed32a068059b45192eeb73d70f0b25931c448
최효재 <hyojae5945@gmail.com>
2024-11-14 13:24:27 +0900

    지도 띄우기

--- vue-passproject/src/main.js
+++ vue-passproject/src/main.js
@@ -1,14 +1,15 @@
-import './assets/main.css'
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import App from "./App.vue";
+import router from "./router";
 
-import App from './App.vue'
-import router from './router'
+import { useKakao } from "vue3-kakao-maps/@utils";
 
-const app = createApp(App)
+useKakao("d02b03ce9a797eb1398d15acce7bfb49");
+const app = createApp(App).mount("#app");
 
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+// app.use(createPinia());
+// app.use(router);
