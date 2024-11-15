552740d5d49a7cc21c4fb1c6a29b93e78804e0b1
Baek Gi Chan <laphayen@gmail.com>
2024-11-15 09:34:28 +0900

    Feat: Spring - 미세먼지 REST API로 변경

--- home/src/main/java/com/ssafy/edu/dust/controller/DustController.java
+++ home/src/main/java/com/ssafy/edu/dust/controller/DustController.java
@@ -1,42 +1,32 @@
 package com.ssafy.edu.dust.controller;
 
-import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
-import com.google.gson.Gson;
 import com.ssafy.edu.dust.model.DustDto;
 import com.ssafy.edu.dust.model.service.DustService;
-
-import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/dustinfo")
 public class DustController {
-	@Autowired
-	private DustService dustService;
 
-	@GetMapping("/dustList")
-	public ModelAndView dustList(ModelAndView mav, HttpServletResponse response) throws IOException {
-		List<DustDto> dustList = dustService.getDustInfoList();
+    @Autowired
+    private DustService dustService;
 
-		mav.addObject("dustList", dustList);
-		mav.setViewName("/views/dustinfo");
-		//모델앤뷰 리턴 부분
-		
-		
-		response.setContentType("application/json");
-		response.setCharacterEncoding("UTF-8");
-		String json = new Gson().toJson(dustList);
-		response.getWriter().write(json);//JSON 
-
-		return mav;
-	}
-
+    @GetMapping("/dustList")
+    public ResponseEntity<List<DustDto>> dustList() {
+        try {
+            List<DustDto> dustList = dustService.getDustInfoList();
+            return new ResponseEntity<>(dustList, HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
