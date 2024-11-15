07851103631f386d2c089cbabd920b8ac5f14721
Baek Gi Chan <laphayen@gmail.com>
2024-11-15 17:55:35 +0900

    Feat: test
    test

--- home/src/main/java/com/ssafy/edu/board/controller/BoardController.java
+++ home/src/main/java/com/ssafy/edu/board/controller/BoardController.java
@@ -20,74 +20,85 @@ import jakarta.servlet.http.HttpSession;
 public class BoardController {
 
     private final BoardService boardService;
-    private static final int DEFAULT_PGNO = 1;
 
     public BoardController(BoardService boardService) {
         this.boardService = boardService;
     }
 
-    // Get list of articles
+    // 글 목록 조회
     @GetMapping
-    public ResponseEntity<Map<String, Object>> listArticle(@RequestParam Map<String, String> paramMap) throws Exception {
-        int pgno = parsePageNumber(paramMap);
-        paramMap.put("pgno", String.valueOf(pgno));
-
-        List<BoardDto> articles = boardService.listArticle(paramMap);
-        PageNavigation navigation = boardService.makePageNavigation(paramMap);
-
-        Map<String, Object> response = new HashMap<>();
-        response.put("articles", articles);
-        response.put("navigation", navigation);
-
-        return ResponseEntity.ok(response);
-    }
-
-    // Get single article details
-    @GetMapping("/{articleNo}")
-    public ResponseEntity<BoardDto> getArticle(@PathVariable int articleNo) throws Exception {
-        BoardDto article = boardService.getArticle(articleNo);
-        return ResponseEntity.ok(article);
-    }
-
-    // Create new article
-    @PostMapping
-    public ResponseEntity<Void> writeArticle(@RequestBody BoardDto boardDto, HttpSession session) throws Exception {
-        MemberDto memberDto = (MemberDto) session.getAttribute("userinfo");
-        if (memberDto != null) {
-            boardDto.setUserId(memberDto.getUserId());
-            boardService.writeArticle(boardDto);
-            return ResponseEntity.status(HttpStatus.CREATED).build();
-        } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
-    }
-
-    // Update existing article
-    @PutMapping("/{articleNo}")
-    public ResponseEntity<Void> modifyArticle(@PathVariable int articleNo, @RequestBody BoardDto boardDto) throws Exception {
-        boardDto.setArticleNo(articleNo);
-        boardService.modifyArticle(boardDto);
-        return ResponseEntity.noContent().build();
-    }
-
-    // Delete an article
-    @DeleteMapping("/{articleNo}")
-    public ResponseEntity<Void> deleteArticle(@PathVariable int articleNo) throws Exception {
-        boardService.deleteArticle(articleNo);
-        return ResponseEntity.noContent().build();
-    }
-
-    // Helper method to parse page number
-    private int parsePageNumber(Map<String, String> paramMap) {
-        int pgno;
+    public ResponseEntity<Map<String, Object>> listArticle(@RequestParam Map<String, String> paramMap) {
         try {
-            pgno = Integer.parseInt(paramMap.getOrDefault("pgno", "1"));
-            if (pgno < 1) {
-                pgno = DEFAULT_PGNO;
-            }
-        } catch (NumberFormatException e) {
-            pgno = DEFAULT_PGNO;
+            List<BoardDto> articles = boardService.listArticle(paramMap);
+            PageNavigation navigation = boardService.makePageNavigation(paramMap);
+
+            Map<String, Object> response = new HashMap<>();
+            response.put("articles", articles);
+            response.put("navigation", navigation);
+
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
-        return pgno;
+    }
+
+    // 특정 글 조회
+    @GetMapping("/{articleNo}")
+    public ResponseEntity<BoardDto> getArticle(@PathVariable int articleNo) {
+        try {
+            BoardDto article = boardService.getArticle(articleNo);
+            if (article != null) {
+                return ResponseEntity.ok(article);
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    // 글 작성
+    @PostMapping
+    public ResponseEntity<Void> writeArticle(@RequestBody BoardDto boardDto, HttpSession session) {
+        try {
+            MemberDto memberDto = (MemberDto) session.getAttribute("userinfo");
+            if (memberDto != null) {
+                boardDto.setUserId(memberDto.getUserId());
+                boardService.writeArticle(boardDto);
+                return ResponseEntity.status(HttpStatus.CREATED).build();
+            } else {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    // 글 수정
+    @PutMapping("/{articleNo}")
+    public ResponseEntity<Void> modifyArticle(@PathVariable int articleNo, @RequestBody BoardDto boardDto) {
+        try {
+            boardDto.setArticleNo(articleNo);
+            boardService.modifyArticle(boardDto);
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    // 글 삭제
+    @DeleteMapping("/{articleNo}")
+    public ResponseEntity<Void> deleteArticle(@PathVariable int articleNo) {
+        try {
+            boardService.deleteArticle(articleNo);
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
